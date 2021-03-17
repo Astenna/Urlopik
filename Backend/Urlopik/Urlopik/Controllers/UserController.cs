8a95c519069f306479cc106f3997c402ae8511ea
Astenna <marekkinga97@gmail.com>
2021-03-17 23:58:03 +0100

    Added gitignore & basic project structure

---
+++ Backend/Urlopik/Urlopik/Controllers/UserController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Urlopik.Controllers
+{
+    [ApiController]
+    [Route("api/users")]
+    public class UserController : ControllerBase
+    {
+       // TODO 
+    }
+}
