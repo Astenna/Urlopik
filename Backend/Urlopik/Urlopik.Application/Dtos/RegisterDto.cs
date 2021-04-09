6f60cc6a5c019b8516b96d4085c2b064220456b7
Astenna <marekkinga97@gmail.com>
2021-04-09 21:23:03 +0200

    Remove reduntant role validation

--- Backend/Urlopik/Urlopik.Application/Dtos/RegisterDto.cs
+++ Backend/Urlopik/Urlopik.Application/Dtos/RegisterDto.cs
@@ -27,36 +27,6 @@ namespace Urlopik.Application.Dtos
             RuleFor(x => x.Password.Length > 6);
             RuleFor(x => x.Password).NotEmpty();
             RuleFor(x => x.Email).NotEmpty();
-            RuleFor(x => x.Role)
-                .Must(x => IsDefined(x))
-                .WithMessage(x => $"{x.Role} is not a valid role, choose from: {GetAvailableRoles()}");
-        }
-
-
-        private bool IsDefined(string value)
-        {
-            try
-            {
-                _ = (Roles)Enum.Parse(typeof(Roles), value);
-            }
-            catch (Exception)
-            {
-                return false;
-            }
-            return true;
-        }
-
-        private string GetAvailableRoles()
-        {
-            StringBuilder result = new StringBuilder();
-
-            foreach (var value in Enum.GetValues(typeof(Roles)))
-            {
-                result.Append(value);
-                result.Append(" ");
-            }
-
-            return result.ToString();
         }
     }
 }
